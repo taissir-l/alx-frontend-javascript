8cc94843254bfce7c4d62b34f8473ab8b93dd1f8
“taissir-l” <taissirlassoued1@gmail.com>
2024-07-23 15:44:21 +0000

    task0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,7 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate an API call
-        const success = true; // This is just for demonstration purposes
-        if (success) {
-            resolve("API call successful");
-        } else {
-            reject("API call failed");
-        }
-    });
+function getFullResponseFromAPI(apiEndpoint) {
+  return new Promise((resolve, reject) => {
+    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
+    reject(Error('Failed to fetch API'));
+  });
 }
-
-export default getResponseFromAPI;
+export default getFullResponseFromAPI;
